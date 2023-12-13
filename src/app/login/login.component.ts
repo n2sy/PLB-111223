f6459af0665b13da3f8411426b41a2965aa3fd5c
n2sy <nidhal.jelassi@fsegt.utm.tn>
2023-12-13 10:06:22 +0100

    use ngForms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,32 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  defaultLang = "php";
 
+  myCommentaire = 'Rien à signaler...'
   submitHandler(f) {
     console.log(f);
-    
+  }
+
+  resetHandler(f : NgForm) {
+    f.reset();
+  }
+
+  genratePwd(f : NgForm) {
+    f.setValue({
+      "login": "",
+      "password": "infoGones123",
+      "infos": {
+        "langage": "",
+        "commentaire": ""
+      },
+      "exemple": ""
+    });
+  }
+  genratePwdII(f : NgForm) {
+    f.form.patchValue({
+      "password": "infoGones123",
+    });
   }
 
 }
